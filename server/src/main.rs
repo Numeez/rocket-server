b674c0ea04e3c60913bf64fe1c8390907d8a1698
dvl-numeez <numeez.baloch@digivatelabs.com>
2025-04-02 10:56:01 +0530

    Playing around with rocket

---
+++ server/src/main.rs
@@ -0,0 +1,27 @@
+#[macro_use] extern crate rocket;
+
+
+#[get("/")]
+fn index()-> &'static str {
+    "Hello Rocket"
+}
+
+#[get("/custom")]
+fn custom_index()-> &'static str {
+    "Hello Server"
+}
+
+#[get("/name/<name>")]
+fn print_name(name:&str)-> String{
+    let mut hello = String::from("Hello ");
+    hello.push_str(name);
+    hello
+}
+
+#[rocket::main]
+async  fn main() -> Result<(),rocket::Error>{
+    let _rocket = rocket::build().mount("/", routes![index,custom_index,print_name])
+    .launch()
+    .await?;
+    Ok(())
+}
